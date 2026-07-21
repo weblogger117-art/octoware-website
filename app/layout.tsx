7520ea2064150da89cfc3a40c27f1be09beab342
weblogger117-art <weblogger117@gmail.com>
2026-07-21 08:37:27 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "easy-soft GmbH",
+  description: "Neue Unternehmenswebsite der easy-soft GmbH"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
