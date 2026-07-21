5ccee56af807ba2214662e1efd6304e9b0f1fe92
weblogger117-art <weblogger117@gmail.com>
2026-07-21 08:58:25 +0200

    Create Section.tsx

---
+++ components/ui/Section.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { Container } from "./Container";
+
+type SectionProps = {
+  children: ReactNode;
+  className?: string;
+  containerClassName?: string;
+};
+
+export function Section({
+  children,
+  className = "",
+  containerClassName = "",
+}: SectionProps) {
+  return (
+    <section className={`py-16 lg:py-24 ${className}`}>
+      <Container className={containerClassName}>
+        {children}
+      </Container>
+    </section>
+  );
+}
