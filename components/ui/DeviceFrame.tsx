94a480743334bb5ec9f14dd7440c1fe4f82d5d55
weblogger117-art <weblogger117@gmail.com>
2026-07-21 14:05:49 +0200

    Update DeviceFrame.tsx

--- components/ui/DeviceFrame.tsx
+++ components/ui/DeviceFrame.tsx
@@ -25,10 +25,10 @@ export function DeviceFrame({
       <div
   className={cn(
     "absolute overflow-hidden",
-    "left-[3.4%]",
-    "right-[2.3%]",
-    "top-[5.3%]",
-    "bottom-[9.1%]",
+    "left-[3.8%]",
+    "right-[3.1%]",
+    "top-[5.6%]",
+    "bottom-[9.8%]",
     screenClassName
   )}
 >
