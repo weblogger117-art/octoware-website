254604e574b338fb797cd5da89913d9f8eab5a45
weblogger117-art <weblogger117@gmail.com>
2026-07-21 11:52:34 +0200

    Update Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,22 +1,16 @@
 import { Section } from "@/components/ui";
 
-import { HeroBackground } from "./HeroBackground";
 import { HeroContent } from "./HeroContent";
 import { HeroVisual } from "./HeroVisual";
 
 export function Hero() {
   return (
-    <Section className="relative overflow-hidden py-24 lg:py-32">
-
-      {/* Hintergrund */}
-      <HeroBackground />
-
-      {/* Inhalt */}
-      <div className="relative z-10 grid items-center gap-20 lg:grid-cols-[0.9fr_1.1fr]">
-        <HeroContent />
-        <HeroVisual />
-      </div>
-
+    <Section
+      className="overflow-hidden py-24 lg:py-32"
+      containerClassName="grid items-center gap-20 lg:grid-cols-[0.9fr_1.1fr]"
+    >
+      <HeroContent />
+      <HeroVisual />
     </Section>
   );
 }
