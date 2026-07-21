e0617c3849e13c0790c5c92759d146dea49d2279
weblogger117-art <weblogger117@gmail.com>
2026-07-21 11:43:20 +0200

    Update HeroVisual.tsx

--- components/sections/HeroVisual.tsx
+++ components/sections/HeroVisual.tsx
@@ -5,17 +5,18 @@ import { DeviceFrame } from "@/components/ui";
 export function HeroVisual() {
   return (
     <div className="flex justify-center lg:justify-end">
-      <DeviceFrame>
-
+      <DeviceFrame
+        className="max-w-[880px]"
+      >
         <Image
-          src="/images/screens/OctoWareNET_Dashboard.png"
+          src="/images/screens/OctowareNET_Dashboard.png"
           alt="Dashboard von OctoWare®NET"
           width={1600}
           height={1000}
           priority
-          className="h-auto w-full"
+          draggable={false}
+          className="h-auto w-full contrast-105 brightness-[0.98]"
         />
-
       </DeviceFrame>
     </div>
   );
