83a7d2dcebccf34c626e2562f3bae646c59caedd
weblogger117-art <weblogger117@gmail.com>
2026-07-21 08:34:26 +0200

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// Diese Datei darf nicht bearbeitet werden.
+// Weitere Informationen:
+// https://nextjs.org/docs/app/api-reference/config/typescript
