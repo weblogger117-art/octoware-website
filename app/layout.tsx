dac6e64995edc43aa9a5d254f1c9aee42b3efc91
weblogger117-art <weblogger117@gmail.com>
2026-07-21 08:48:06 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
-import "./globals.css";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: "easy-soft GmbH",
-  description: "Neue Unternehmenswebsite der easy-soft GmbH"
+  title: {
+    default: "easy-soft GmbH",
+    template: "%s | easy-soft GmbH",
+  },
+  description: "Neue Unternehmenswebsite der easy-soft GmbH",
 };
 
 export default function RootLayout({
@@ -13,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="de">
-      <body>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
