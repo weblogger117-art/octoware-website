e54d68184b2df3b2facc8f6b4aa6314276dedcbb
weblogger117-art <weblogger117@gmail.com>
2026-07-21 15:41:06 +0200

    Create Navigation.tsx

---
+++ components/layout/Navigation.tsx
@@ -0,0 +1,19 @@
+import { NavItem } from "./NavItem";
+
+export function Navigation() {
+  return (
+    <nav className="hidden items-center gap-10 lg:flex">
+      <NavItem href="/plattform">Plattform</NavItem>
+
+      <NavItem href="/fachverfahren">Fachverfahren</NavItem>
+
+      <NavItem href="/referenzen">Referenzen</NavItem>
+
+      <NavItem href="/aktuelles">Aktuelles</NavItem>
+
+      <NavItem href="/unternehmen">Unternehmen</NavItem>
+
+      <NavItem href="/easy-academy">easy-Academy</NavItem>
+    </nav>
+  );
+}
