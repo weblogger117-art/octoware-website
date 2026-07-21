cf9638f44f7661abaaded5b2fed023222b82ac1d
weblogger117-art <weblogger117@gmail.com>
2026-07-21 12:10:09 +0200

    Update Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -16,11 +16,11 @@ py-24
 lg:py-32
 "
       containerClassName="
-        grid
-        items-center
-        gap-20
-        lg:grid-cols-[0.85fr_1.15fr]
-      "
+grid
+items-center
+gap-16
+lg:grid-cols-[0.82fr_1.18fr]
+"
     >
       <HeroContent />
       <HeroVisual />
