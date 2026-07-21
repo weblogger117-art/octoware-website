07561dff2782d0322164b46328f117f5165af240
weblogger117-art <weblogger117@gmail.com>
2026-07-21 10:14:03 +0200

    Update Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -6,7 +6,16 @@ import { HeroVisual } from "./HeroVisual";
 export function Hero() {
   return (
     <Section
-      className="overflow-hidden py-24 lg:py-32"
+      className="
+relative
+overflow-hidden
+bg-gradient-to-b
+from-white
+via-white
+to-slate-50
+py-24
+lg:py-32
+"
       containerClassName="grid items-center gap-20 lg:grid-cols-2"
     >
       <HeroContent />
