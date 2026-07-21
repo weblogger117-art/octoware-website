43dc1939636f900c52c2fa6fcd7ed26ea2b687a9
weblogger117-art <weblogger117@gmail.com>
2026-07-21 09:50:36 +0200

    Update HeroContent.tsx

--- components/sections/HeroContent.tsx
+++ components/sections/HeroContent.tsx
@@ -7,11 +7,11 @@ export function HeroContent() {
       <Badge>NEXT GENERATION</Badge>
 
       <Heading
-  as="h1"
-  className="mt-8 max-w-2xl"
-  title="Die Softwareplattform für den öffentlichen Gesundheitsdienst."
-  description="OctoWare®NET vereint moderne Fachverfahren, digitale Bürgerdienste und effiziente Prozesse auf einer zentralen Plattform."
-/>
+        className="mt-8"
+        as="h1"
+        title="Die Softwareplattform für den öffentlichen Gesundheitsdienst."
+        description="OctoWare®NET vereint moderne Fachverfahren, digitale Bürgerdienste und effiziente Prozesse auf einer zentralen Plattform."
+      />
 
       <div className="mt-10 flex flex-wrap gap-4">
         <LinkButton href="/plattform">
