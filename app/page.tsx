e954ced09ecc1ba39d18c7e619a471a0263f0a16
weblogger117-art <weblogger117@gmail.com>
2026-07-21 08:37:52 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>OctoWare® Website Foundation</h1>
+      <p>Die Foundation wurde erfolgreich erstellt.</p>
+    </main>
+  );
+}
