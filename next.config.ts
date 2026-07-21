a9ec85d0a679eed5e4b048fe7e143f6dacb0cb6f
weblogger117-art <weblogger117@gmail.com>
2026-07-21 08:33:14 +0200

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  poweredByHeader: false,
+
+  compress: true,
+};
+
+export default nextConfig;
