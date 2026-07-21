e0f1710d618ec0253c97951c1523c79252b6e032
weblogger117-art <weblogger117@gmail.com>
2026-07-21 11:26:04 +0200

    Update HeroVisual.tsx

--- components/sections/HeroVisual.tsx
+++ components/sections/HeroVisual.tsx
@@ -8,7 +8,7 @@ export function HeroVisual() {
       <DeviceFrame>
 
         <Image
-          src="/images/screens/OctowareNET_Dashboard.png"
+          src="/public/images/screens/OctowareNET_Dashboard.png"
           alt="Dashboard von OctoWare®NET"
           width={1600}
           height={1000}
