2be8b9037c9c4ed74bef6660bde261b8c445889f
weblogger117-art <weblogger117@gmail.com>
2026-07-22 13:15:19 +0200

    Update index.ts

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,5 +2,6 @@ export * from "./Badge";
 export * from "./Button";
 export * from "./Container";
 export * from "./DeviceFrame";
+export { FeatureCard } from "./FeatureCard";
 export * from "./Laptop";
 export * from "./Section";
