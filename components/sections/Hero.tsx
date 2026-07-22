8ed0d475b67558989fb2f413db745b6056d4fc2c
weblogger117-art <weblogger117@gmail.com>
2026-07-22 11:13:28 +0200

    Update Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -17,6 +17,21 @@ export function Hero() {
         lg:py-36
       "
     >
+      {/* Sanfter Übergang zur nächsten Section */}
+<div
+  aria-hidden
+  className="
+    pointer-events-none
+    absolute
+    inset-x-0
+    bottom-0
+    h-40
+    bg-gradient-to-b
+    from-transparent
+    to-white
+    z-10
+  "
+/>
       {/* Linke Seite */}
       <div className="relative z-20 max-w-[620px]">
         <HeroContent />
