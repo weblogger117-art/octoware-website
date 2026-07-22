597e428cf89ab3bfc65348e224e519a87ba428d5
weblogger117-art <weblogger117@gmail.com>
2026-07-22 09:54:42 +0200

    Update NetworkBackground.tsx

--- components/graphics/NetworkBackground.tsx
+++ components/graphics/NetworkBackground.tsx
@@ -1,39 +1,45 @@
-import React from "react";
 export function NetworkBackground() {
   return (
     <svg
-      viewBox="0 0 1200 700"
+      viewBox="0 0 1400 900"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className="w-full h-auto"
     >
-      <g
+    {/* Hauptlinien */}
+<g
   stroke="#146ab1"
-  strokeOpacity="0.12"
-  strokeWidth="2"
+  strokeOpacity="0.18"
+  strokeWidth="2.2"
   fill="none"
   strokeLinecap="round"
 >
-  <path d="M120 260 C260 120 520 120 700 240" />
+  <path d="M760 120 C900 70 1160 80 1320 180" />
+  <path d="M700 330 C920 250 1180 290 1360 430" />
+  <path d="M680 560 C900 500 1180 540 1350 670" />
 </g>
-      <g>
-  {/* Große Knoten */}
-  <circle cx="120" cy="120" r="7" fill="#146ab1" />
-  <circle cx="520" cy="200" r="7" fill="#146ab1" />
-  <circle cx="980" cy="380" r="7" fill="#146ab1" />
-
-  {/* Mittlere Knoten */}
-  <circle cx="320" cy="120" r="5" fill="#146ab1" />
-  <circle cx="760" cy="180" r="5" fill="#146ab1" />
-  <circle cx="220" cy="360" r="5" fill="#146ab1" />
-  <circle cx="480" cy="360" r="5" fill="#146ab1" />
-  <circle cx="720" cy="300" r="5" fill="#146ab1" />
-
-  {/* Akzentknoten */}
-  <circle cx="980" cy="260" r="5" fill="#adce00" />
+      {/* Nebenlinien */}
+<g
+  stroke="#146ab1"
+  strokeOpacity="0.10"
+  strokeWidth="1.2"
+  fill="none"
+  strokeLinecap="round"
+>
+  <path d="M780 150 C960 120 1160 160 1310 250" />
+  <path d="M720 390 C920 340 1160 380 1330 520" />
 </g>
-
-      {/* Knoten kommen im nächsten Schritt */}
+      {/* Datenpfade */}
+<g
+  stroke="#146ab1"
+  strokeOpacity="0.08"
+  strokeWidth="1"
+  strokeDasharray="4 6"
+  fill="none"
+>
+  <path d="M790 180 C960 140 1180 180 1340 300" />
+  <path d="M730 450 C930 390 1170 450 1330 610" />
+</g>
     </svg>
   );
 }
