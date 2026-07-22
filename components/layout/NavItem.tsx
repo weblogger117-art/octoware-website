8db36540ff9a729c27119cd511afedfb7cd8e668
weblogger117-art <weblogger117@gmail.com>
2026-07-22 08:48:29 +0200

    Update NavItem.tsx

--- components/layout/NavItem.tsx
+++ components/layout/NavItem.tsx
@@ -8,19 +8,38 @@ type NavItemProps = {
 
 export function NavItem({ href, children }: NavItemProps) {
   return (
-    <Link
-      href={href}
-      className="
-        relative
-        text-[15px]
-        font-medium
-        text-slate-700
-        transition-colors
-        duration-200
-        hover:text-[#146ab1]
-      "
-    >
-      {children}
-    </Link>
+   <Link
+  href={href}
+  className="
+    group
+    relative
+    inline-flex
+    items-center
+    text-[15px]
+    font-medium
+    text-slate-700
+    transition-colors
+    duration-300
+    hover:text-[#146ab1]
+  "
+>
+  <span>{children}</span>
+
+  <span
+    className="
+      absolute
+      -bottom-1
+      left-1/2
+      h-0.5
+      w-0
+      -translate-x-1/2
+      rounded-full
+      bg-[#146ab1]
+      transition-all
+      duration-300
+      group-hover:w-full
+    "
+  />
+</Link>
   );
 }
