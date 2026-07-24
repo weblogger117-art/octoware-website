dc46a0ad1fd80af5e2144bc1be8556684a09d930
weblogger117-art <weblogger117@gmail.com>
2026-07-24 16:06:35 +0200

    Update index.ts

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -2,3 +2,4 @@ export * from "./Hero";
 export { Highlights } from "./Highlights";
 export { WhyOctoWare } from "./WhyOctoWare";
 export { default as Modules } from "./Modules";
+export { default as Workflow } from "./Workflow";
