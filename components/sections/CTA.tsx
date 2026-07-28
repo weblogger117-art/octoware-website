3cacf76a2cc914a71d2d9feac590ae9e2e432237
weblogger117-art <weblogger117@gmail.com>
2026-07-28 16:28:30 +0200

    Create CTA.tsx

---
+++ components/sections/CTA.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import { LinkButton } from "@/components/ui";
+
+export default function CTA() {
+  return (
+    <section className="relative overflow-hidden py-28">
+      <div className="mx-auto max-w-7xl px-6">
+
+      </div>
+    </section>
+  );
+}
