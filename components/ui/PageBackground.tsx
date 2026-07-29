f1fc5d71a37310e9248d631cf6532822fc4d5a03
weblogger117-art <weblogger117@gmail.com>
2026-07-29 14:38:18 +0200

    Update PageBackground.tsx

--- components/ui/PageBackground.tsx
+++ components/ui/PageBackground.tsx
@@ -36,8 +36,8 @@ export function PageBackground({
           left-1/2
           top-[220px]
 
-          h-[900px]
-          w-[900px]
+          h-[1100px]
+          w-[1100px]
 
           -translate-x-1/2
 
@@ -50,7 +50,53 @@ export function PageBackground({
           -z-10
         "
       />
+      
+{/* Secondary Blue Glow */}
+      <div
+  aria-hidden
+  className="
+    pointer-events-none
+    absolute
 
+    right-[-220px]
+    top-[1150px]
+
+    h-[700px]
+    w-[700px]
+
+    rounded-full
+
+    bg-[#146ab1]/6
+
+    blur-[180px]
+
+    -z-10
+  "
+/>
+
+      {/* Green Glow */}
+<div
+  aria-hidden
+  className="
+    pointer-events-none
+    absolute
+
+    left-[-180px]
+    bottom-[260px]
+
+    h-[650px]
+    w-[650px]
+
+    rounded-full
+
+    bg-[#adce00]/12
+
+    blur-[170px]
+
+    -z-10
+  "
+/>
+      
       {children}
 
     </div>
