7086ea2044c807c874e445b9228e4b0a1f595a17
weblogger117-art <weblogger117@gmail.com>
2026-07-29 14:23:05 +0200

    Update HeroVisual.tsx

--- components/sections/HeroVisual.tsx
+++ components/sections/HeroVisual.tsx
@@ -18,45 +18,10 @@ export function HeroVisual() {
   "
 >
       {/* Netzwerkgrafik */}
-      <Image
-        src="/images/graphics/network-background.svg"
-        alt=""
-        aria-hidden="true"
-        width={1800}
-        height={1100}
-        draggable={false}
-        className="
-          absolute
-          left-1/2
-          top-1/2
-          w-[1550px]
-          max-w-none
-          -translate-x-1/2
-          -translate-y-1/2
-          opacity-90
-          -z-20
-          select-none
-        "
-      />
+     
 
       {/* Licht */}
-      <div
-        aria-hidden
-        className="
-          absolute
-          left-1/2
-          top-1/2
-          h-[860px]
-          w-[860px]
-          -translate-x-1/2
-          -translate-y-1/2
-          rounded-full
-          bg-[#c5d7ed]
-          opacity-35
-          blur-[150px]
-          -z-10
-        "
-      />
+      
 
       {/* Gerätegruppe */}
      <div
