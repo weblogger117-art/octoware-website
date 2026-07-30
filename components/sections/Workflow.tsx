fb958c26b49adf278cc666c567230e5e2a46d890
weblogger117-art <weblogger117@gmail.com>
2026-07-30 10:26:56 +0200

    Update Workflow.tsx

--- components/sections/Workflow.tsx
+++ components/sections/Workflow.tsx
@@ -63,7 +63,7 @@ useEffect(() => {
   return (
     <section
   ref={sectionRef}
-  className="relative overflow-hidden py-32"
+  className="relative py-32"
 >
       <div className="mx-auto max-w-7xl px-6">
 
