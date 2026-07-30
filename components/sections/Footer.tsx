b37cea25545e24355ce61af350154fef9c7836f9
weblogger117-art <weblogger117@gmail.com>
2026-07-30 16:05:16 +0200

    Update Footer.tsx

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { SectionGlow } from "@/components/ui";
+
 export function Footer() {
   return (
     <footer
