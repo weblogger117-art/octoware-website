93dbfea2faf687a07a1a09e5d844ce2820dbd3cb
weblogger117-art <weblogger117@gmail.com>
2026-08-03 11:04:21 +0200

    Update Footer.tsx

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -320,6 +320,78 @@ export function Footer() {
   </div>
 
 </div>
+
+      <div>
+
+  <h4
+    className="
+      text-sm
+      font-semibold
+      uppercase
+      tracking-[0.18em]
+      text-[#146ab1]
+    "
+  >
+    Kontakt
+  </h4>
+
+  <div className="mt-6 flex flex-col gap-4">
+
+    <Link
+      href="/kontakt"
+      className="
+        text-slate-600
+        transition-all
+        duration-200
+        hover:translate-x-1
+        hover:text-[#146ab1]
+      "
+    >
+      Kontakt
+    </Link>
+
+    <Link
+      href="/support"
+      className="
+        text-slate-600
+        transition-all
+        duration-200
+        hover:translate-x-1
+        hover:text-[#146ab1]
+      "
+    >
+      Support
+    </Link>
+
+    <Link
+      href="/kundenportal"
+      className="
+        text-slate-600
+        transition-all
+        duration-200
+        hover:translate-x-1
+        hover:text-[#146ab1]
+      "
+    >
+      Kundenportal
+    </Link>
+
+    <Link
+      href="/fernwartung"
+      className="
+        text-slate-600
+        transition-all
+        duration-200
+        hover:translate-x-1
+        hover:text-[#146ab1]
+      "
+    >
+      Fernwartung
+    </Link>
+
+  </div>
+
+</div>
       
       </div>
     </div>
