8588682183d638e6c3eee99b3764d564ce2bd0bc
weblogger117-art <weblogger117@gmail.com>
2026-08-07 09:49:31 +0200

    Update page.tsx

--- app/plattform/page.tsx
+++ app/plattform/page.tsx
@@ -1,10 +1,12 @@
 
 import PlatformHero from "@/components/sections/platform/Hero";
+import BrowserBased from "@/components/sections/platform/BrowserBased";
 
 export default function PlattformPage() {
   return (
     <main>
       <PlatformHero />
+      <BrowserBased />
     </main>
   );
 }
