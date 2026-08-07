ce7ed72ce13a60dfd2d41220d85d41b92a960f88
weblogger117-art <weblogger117@gmail.com>
2026-08-07 09:56:44 +0200

    Update BrowserBased.tsx

--- components/sections/platform/BrowserBased.tsx
+++ components/sections/platform/BrowserBased.tsx
@@ -1,7 +1,23 @@
 export default function BrowserBased() {
   return (
-    <section className="py-32">
+    <section className="py-32 bg-white">
       <div className="mx-auto max-w-7xl px-6">
+
+        <div className="grid items-center gap-20 lg:grid-cols-2">
+
+          {/* Linke Seite */}
+
+          <div>
+
+          </div>
+
+          {/* Rechte Seite */}
+
+          <div>
+
+          </div>
+
+        </div>
 
       </div>
     </section>
